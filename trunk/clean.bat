08899d1ef7a05484774619aa3b83c9f05cb933b2
Kromster80 <Kromster80@2c8d9ed8-09bc-11df-8158-c7cbc0dd6240>
2012-01-06 09:25:23 +0000

    Added clean.bat

---
+++ trunk/clean.bat
@@ -0,0 +1,6 @@
+erase /F /Q /S *.~* *.ddp *.drc *.dcp *.dcu
+erase /F /Q /S *.o *.or *.ppu *.compiled
+erase /F /Q /S *.log *.tmp thumbs*.db *.ion *.skincfg
+erase /F /Q /S /A:H *.~* *.ddp *.drc *.dcp *.dcu
+erase /F /Q /S /A:H *.o *.or *.ppu *.compiled
+erase /F /Q /S /A:H *.log *.tmp thumbs.db *.ion *.skincfg
