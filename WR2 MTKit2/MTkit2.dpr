d96498c8fc35a4069c5614ff174aa88443b8d2cb
kromster80 <kromster80@gmail.com>
2022-09-08 08:51:47 +0300

    Moved source into own folder for neats

--- WR2 MTKit2/MTkit2.dpr
+++ WR2 MTKit2/MTkit2.dpr
@@ -1,11 +1,11 @@
 program MTkit2;
 uses
   Forms,
-  MTkit2_Unit1 in 'MTkit2_Unit1.pas' {Form1},
-  MTkit2_Defaults in 'MTkit2_Defaults.pas',
-  MTkit2_IO in 'MTkit2_IO.pas',
-  MTkit2_Render in 'MTkit2_Render.pas',
-  MTkit2_RenderLegacy in 'MTkit2_RenderLegacy.pas',
+  MTkit2_Unit1 in 'src\MTkit2_Unit1.pas' {Form1},
+  MTkit2_Defaults in 'src\MTkit2_Defaults.pas',
+  MTkit2_IO in 'src\MTkit2_IO.pas',
+  MTkit2_Render in 'src\MTkit2_Render.pas',
+  MTkit2_RenderLegacy in 'src\MTkit2_RenderLegacy.pas',
   KromUtils in '..\_Common_\KromUtils.pas',
   ColorPicker in '..\_Common_\ColorPicker.pas' {Form2};
 
